b9fd996edc9750fff221345e779bfbaa7efc8069
刘子健_LiuZijian <alphatk@vip.qq.com>
2025-10-04 14:35:11 +0800

    更新数据集下载地址到OneDrive

--- data/TaxiBJ/TaxiBJDataModule.py
+++ data/TaxiBJ/TaxiBJDataModule.py
@@ -86,33 +86,3 @@ class TaxibjDataset(Dataset):
             data = seqs[:len_data, ...]
             labels = seqs[len_data:, ...]
         return data, labels
-
-
-# class TaxibjDataset(Dataset):
-#     """Taxibj <https://arxiv.org/abs/1610.00081>`_ Dataset"""
-#
-#     def __init__(self, X, Y, use_augment=False, data_name='taxibj'):
-#         super(TaxibjDataset, self).__init__()
-#         self.X = (X + 1) / 2  # channel is 2
-#         self.Y = (Y + 1) / 2
-#         self.use_augment = use_augment
-#         self.mean = 0
-#         self.std = 1
-#         self.data_name = data_name
-#
-#     def _augment_seq(self, seqs):
-#         """Augmentations as a video sequence"""
-#         # Random Flip
-#         if random.randint(0, 1):
-#             seqs = torch.flip(seqs, dims=(3,))  # horizontal flip
-#         return seqs
-#
-#     def __len__(self):
-#         return self.X.shape[0]
-#
-#     def __getitem__(self, index):
-#         data = torch.tensor(self.X[index, ::]).float()
-#         data = torch.flip(data, dims=[0])
-#         labels = torch.tensor(self.Y[index, ::]).float()
-#
-#         return data, labels
